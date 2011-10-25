5c694196e55c5cdc926264724eb3420ff538b877
Juan Maria Lao <juanlao@gmail.com>
2011-10-25 17:51:55 +0200

    Containers Tutorial Structured

---
+++ Containers/Sources/BorderLayout/MyApplication.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Syderis.CellSDK.Core;
+using Syderis.CellSDK.Core.Controls;
+using Microsoft.Xna.Framework;
+using Syderis.CellSDK.Core.Layouts;
+
+namespace BorderLayoutSample
+{
+    class MyApplication : MultitouchApplication
+    {
+        /// <summary>
+        /// The main method for loading controls and resources.
+        /// </summary>
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            Label lblCenter;
+            Label lblEast;
+            Label lblNorth;
+            Label lblSouth;
+            Label lblWest;
+
+            lblCenter = new Label("Center");
+            lblEast = new Label("East");
+            lblNorth = new Label("North");
+            lblSouth = new Label("South");
+            lblWest = new Label("West");
+
+            lblCenter.Align = Label.AlignType.MIDDLECENTER;
+            lblEast.Align = Label.AlignType.MIDDLECENTER;
+            lblNorth.Align = Label.AlignType.MIDDLECENTER;
+            lblSouth.Align = Label.AlignType.MIDDLECENTER;
+            lblWest.Align = Label.AlignType.MIDDLECENTER;
+
+            Container<BorderLayout> borderContainer = new Container<BorderLayout>(new BorderLayout());
+            borderContainer.BackgroundColor = Color.Transparent;
+            borderContainer.Layout.AddComponent(BorderLayout.Organization.CENTER, lblCenter);
+            borderContainer.Layout.AddComponent(BorderLayout.Organization.EAST, lblEast);
+            borderContainer.Layout.AddComponent(BorderLayout.Organization.NORTH, 25, lblNorth);
+            borderContainer.Layout.AddComponent(BorderLayout.Organization.SOUTH, 25, lblSouth);
+            borderContainer.Layout.AddComponent(BorderLayout.Organization.WEST, lblWest);
+
+            AddComponent(borderContainer, 10, 10);
+            borderContainer.Size = new Vector2(400, 200);
+
+        }
+    }
+}
---
+++ Containers/Sources/GridLayout/MyApplication.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Syderis.CellSDK.Core;
+using Syderis.CellSDK.Core.Controls;
+using Syderis.CellSDK.Core.Layouts;
+using Microsoft.Xna.Framework;
+
+namespace GridLayoutSample
+{
+    class MyApplication : MultitouchApplication
+    {
+        /// <summary>
+        /// The main method for loading controls and resources.
+        /// </summary>
+        public override void Initialize()
+        {
+            base.Initialize();
+            // TODO: Replace these comments with your own poetry, and enjoy!
+            //Add your code here
+            Container<GridLayout> gridContainer = new Container<GridLayout>(new GridLayout(6, 2, 10, 10));
+            gridContainer.BackgroundColor = Color.Transparent;
+            gridContainer.Layout.AddComponent(new Label("One"));
+            gridContainer.Layout.AddComponent(new Label("Two"));
+            gridContainer.Layout.AddComponent(new Label("Three"));
+            gridContainer.Layout.AddComponent(new Label("Four"));
+            gridContainer.Layout.AddComponent(new Label("Five"));
+            gridContainer.Layout.AddComponent(new Label("Six"));
+            gridContainer.Layout.AddComponent(new Label("Seven"));
+            gridContainer.Layout.AddComponent(new Label("Eight"));
+            gridContainer.Layout.AddComponent(new Label("Nine"));
+            gridContainer.Layout.AddComponent(new Label("Ten"));
+            gridContainer.Layout.AddComponent(new Label("Eleven"));
+            gridContainer.Layout.AddComponent(new Label("Twelve"));
+            gridContainer.Size = new Vector2(400, 400);
+            AddComponent(gridContainer, 50, 50);
+
+        }
+    }
+}
