83bba205e26dd7fabfada02aa2ce8980d4d97346
Juan Maria Lao <juanlao@gmail.com>
2011-10-26 16:04:55 +0200

    Deleted files

--- Custom Component/WP7ComboBox/WP7ComboBox/Program.cs
+++ Custom Component/WP7ComboBox/WP7ComboBox/Program.cs
@@ -1,9 +1,9 @@
 using System;
 
 using Syderis.CellSDK.WindowsPhone.Launcher;
-using ComboBo;
 
-namespace WP7ComboBox
+
+namespace ComboBo
 {
     public class Program : Kernel
     {
@@ -12,7 +12,7 @@ namespace WP7ComboBox
         /// </summary>
         protected override void Initialize()
         {
-            MyApplication application = new MyApplication();
+            Application application = new Application();
             base.Application = application;
             base.Initialize();
         }
