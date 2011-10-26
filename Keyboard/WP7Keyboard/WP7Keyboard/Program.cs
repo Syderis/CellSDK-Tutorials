32ae98172197ad622d58a2d7d10c1531dde685df
Juan Maria Lao <juanlao@gmail.com>
2011-10-26 16:23:31 +0200

    Backbutton implemented

--- Keyboard/WP7Keyboard/WP7Keyboard/Program.cs
+++ Keyboard/WP7Keyboard/WP7Keyboard/Program.cs
@@ -1,9 +1,9 @@
 using System;
 
 using Syderis.CellSDK.WindowsPhone.Launcher;
-using TextAreaSample;
 
-namespace WP7Keyboard
+
+namespace TextAreaSample
 {
     public class Program : Kernel
     {
@@ -12,7 +12,7 @@ namespace WP7Keyboard
         /// </summary>
         protected override void Initialize()
         {
-            MyApplication application = new MyApplication();
+            Application application = new Application();
             base.Application = application;
             base.Initialize();
         }
