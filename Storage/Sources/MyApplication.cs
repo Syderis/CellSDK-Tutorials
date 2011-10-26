b5364b2cd35a588f170475709fe6f9caa3eb8b16
Juan Maria Lao <juanlao@gmail.com>
2011-10-26 11:21:59 +0200

    Storage Tutorial structured

--- Storage/Sources/MyApplication.cs
+++ Storage/Sources/MyApplication.cs
@@ -11,7 +11,7 @@ using Syderis.CellSDK.Core.Storage;
 
 namespace StorageSample
 {
-    class Application : MultitouchApplication
+    class MyApplication : MultitouchApplication
     {
         Button btGetNotice;
 
--- Storage/WP7Storage/WP7Storage/Program.cs
+++ Storage/WP7Storage/WP7Storage/Program.cs
@@ -1,8 +1,9 @@
 using System;
 
 using Syderis.CellSDK.WindowsPhone.Launcher;
+using StorageSample;
 
-namespace StorageSample
+namespace WP7Storage
 {
     public class Program : Kernel
     {
@@ -11,7 +12,7 @@ namespace StorageSample
         /// </summary>
         protected override void Initialize()
         {
-            Application application = new Application();
+            MyApplication application = new MyApplication();
             base.Application = application;
             base.Initialize();
         }
