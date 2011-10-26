b3cb4823c29f62434b7f5fc9f61ef1818594b5c5
Juan Maria Lao <juanlao@gmail.com>
2011-10-26 11:10:05 +0200

    Sounds tutorial Structured

---
+++ Sounds/AndroidSounds/Application.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Syderis.CellSDK.Core;
+using Syderis.CellSDK.Core.Controls;
+
+namespace AndroidSounds
+{
+    class Application : MultitouchApplication
+    {
+        /// <summary>
+        /// The main method for loading controls and resources.
+        /// </summary>
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            // TODO: Replace these comments with your own poetry, and enjoy!
+            //AddComponent(new Label("Hello, World!"), 0, 0);
+        }
+    }
+}
