956e00ba1dcebc2500fa6b4f520ee6d0121fa4d8
Juan Maria Lao <juanlao@gmail.com>
2011-10-28 14:57:09 +0200

    Physics Tutorial for WP7 and Android corrected

--- Physics/WP7Physics/WP7Physics/Program.cs
+++ Physics/WP7Physics/WP7Physics/Program.cs
@@ -3,7 +3,7 @@ using System;
 using Syderis.CellSDK.WindowsPhone.Launcher;
 using Physics;
 
-namespace WP7Physics
+namespace Physics
 {
     public class Program : Kernel
     {
@@ -12,7 +12,7 @@ namespace WP7Physics
         /// </summary>
         protected override void Initialize()
         {
-            MyApplication application = new MyApplication();
+            Application application = new Application();
             base.Application = application;
             base.Initialize();
         }
