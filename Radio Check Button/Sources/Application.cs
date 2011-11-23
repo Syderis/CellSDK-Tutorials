63937b6f756170dfd1291d5734294d302f9d9017
Jorgemagic <jcanton@syderis.com>
2011-11-23 15:08:11 +0100

    - Port RadioCheckButtons tutorials to 1.0-alpha version

--- Radio Check Button/Sources/Application.cs
+++ Radio Check Button/Sources/Application.cs
@@ -10,7 +10,7 @@ using Syderis.CellSDK.Core.Graphics;
 
 namespace SelectOptions
 {
-    class Application : MultitouchApplication
+    class Application : MobileApplication
     {
         Label lbl,lblBlue,lblRed;
         RadioButton rbBlue,rbRed;
