338c5f554fb7c69a6438684a00b45d2a75e98fe4
Jorgemagic <jcanton@syderis.com>
2011-11-23 15:25:19 +0100

    - Port Physic tutorial to 1.0alpha version

--- Physics/Sources/Application.cs
+++ Physics/Sources/Application.cs
@@ -7,10 +7,11 @@ using Syderis.CellSDK.Core;
 using Syderis.CellSDK.Core.Controls;
 using Syderis.CellSDK.Core.Graphics;
 using Microsoft.Xna.Framework;
+using Syderis.CellSDK.Core.Physics;
 
 namespace Physics
 {
-    class Application : MultitouchApplication
+    class Application : MobileApplication
     {
         Label lbl1k, lbl2k, staticLabel;
         /// <summary>
@@ -21,43 +22,44 @@ namespace Physics
             base.Initialize();
 
             // TODO: Replace these comments with your own poetry, and enjoy!
-            SetBackground(Image.CreateImage("background"), true);
+            SetBackground(Image.CreateImage("background"), Adjustment.STRETCH);
 
-            CreatePhysicWorld(new Vector2(0, -10), true, Vector2.Zero);
+            CreatePhysicWorld(new Vector2(0, -10), true, false, Vector2.Zero);            
 
             lbl1k = new Label(Image.CreateImage("1kg"));
             AddComponent(lbl1k, 50, 400);
-            lbl1k.ComponentPhysics.Mass = 1f;
+            PhysicWorld.AddBody(lbl1k);
+            lbl1k.PhysicBody.Mass = 1f;            
 
             lbl2k = new Label(Image.CreateImage("2kg"));
             AddComponent(lbl2k, 200, 400);
-            lbl2k.ComponentPhysics.Mass = 2f;
+            PhysicWorld.AddBody(lbl2k);
+            lbl2k.PhysicBody.Mass = 2f;            
 
-            staticLabel = new Label("I am a static label");
-            AddComponent(staticLabel, 50, 500, PhysicPrimitive.Square, PhysicType.Static);
+            staticLabel = new Label("I am a static label");            
+            AddComponent(staticLabel, 50, 500, BodyShape.SQUARE, BodyType.STATIC, Category.Cat1);
 
             Button btn = new Button("Press me!");
-            AddComponent(btn, 50, 700, PhysicPrimitive.Square, PhysicType.Static);
+            AddComponent(btn, 50, 700, BodyShape.SQUARE, BodyType.STATIC, Category.Cat1);
             btn.Released += new Component.ComponentEventHandler(btn_Released);
 
             staticLabel.Rotation = MathHelper.ToRadians(20f);
-
-            lbl1k.ComponentPhysics.ComponentFixture.Friction = 1f;
-            lbl2k.ComponentPhysics.ComponentFixture.Friction = 10f;
-
+            lbl1k.PhysicBody.Friction = 1f;
+            lbl2k.PhysicBody.Friction = 10f;
+            
             lbl1k.Draggable = true;
             lbl2k.Draggable = true;
 
-            lbl1k.ComponentPhysics.Restitution = 1.2f;
-            lbl2k.ComponentPhysics.Restitution = 0.9f;
-
+            lbl1k.PhysicBody.Restitution = 1.2f;
+            lbl2k.PhysicBody.Restitution = 0.9f;
+            
         }
 
         void btn_Released(Component source)
         {
             Vector2 velocity = new Vector2(0, 10);
-            lbl1k.ComponentPhysics.ComponentFixture.Body.ApplyLinearImpulse(ref velocity);
-            lbl2k.ComponentPhysics.ComponentFixture.Body.ApplyLinearImpulse(ref velocity);
+            lbl1k.PhysicBody.ApplyForce(ActionType.IMPULSE, velocity);
+            lbl2k.PhysicBody.ApplyForce(ActionType.IMPULSE, velocity);         
         }
 
         public override void BackButtonPressed()
