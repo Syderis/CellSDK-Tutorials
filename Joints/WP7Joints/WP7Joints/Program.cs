8fff0051b84666699f0c455a0edb501313f9b359
Juan María Laó Ramos <juanlao@gmail.com>
2011-11-29 11:02:32 +0100

    Joints Tutorial for IOS

---
+++ Joints/WP7Joints/WP7Joints/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+using Syderis.CellSDK.WindowsPhone.Launcher;
+
+namespace Joints
+{
+    public class Program : Kernel
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        protected override void Initialize()
+        {
+            Application application = new Application();
+            FramesPerSecond = 50;
+            base.Application = application;
+            base.Initialize();
+        }
+    }
+}
+
