5afa4c59495748ef013ba01071bd1ff2ea3bdfc7
Juan María Laó Ramos <juanlao@gmail.com>
2011-12-02 14:33:52 +0100

    iOSGPS

--- GPS/Sources/Application.cs
+++ GPS/Sources/Application.cs
@@ -9,7 +9,7 @@ using Syderis.CellSDK.IO.LocationSystem;
 using Syderis.CellSDK.Common;
 using Syderis.CellSDK.Core.Graphics;
 
-namespace WP7GPS
+namespace GPS
 {
     public class Application : MobileApplication
     {
@@ -31,7 +31,7 @@ namespace WP7GPS
             AddComponent(lblLatitude, Preferences.Width / 4, Preferences.Height / 8);
             AddComponent(lblLongitude, Preferences.Width / 4, Preferences.Height / 4);
 
-            LocationSensor.Instance.Start(LocationSensors.GPS);
+            Syderis.CellSDK.IO.LocationSystem.LocationSensor.Instance.Start(Syderis.CellSDK.Common.LocationSensor.GPS);
         }
 
         public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
@@ -41,8 +41,8 @@ namespace WP7GPS
             timer += gameTime.ElapsedGameTime;
             if (timer > TimeSpan.FromSeconds(1))
             {
-                lblLatitude.Text = string.Format("Latitude: {0}", LocationSensor.Instance.GeoLocation.latitude);
-                lblLongitude.Text = string.Format("Longitude: {0}", LocationSensor.Instance.GeoLocation.longitude);
+                lblLatitude.Text = string.Format("Latitude: {0}", Syderis.CellSDK.IO.LocationSystem.LocationSensor.Instance.GeoLocation.latitude);
+                lblLongitude.Text = string.Format("Longitude: {0}", Syderis.CellSDK.IO.LocationSystem.LocationSensor.Instance.GeoLocation.longitude);
                 timer = TimeSpan.Zero;
             }
 
@@ -54,7 +54,7 @@ namespace WP7GPS
         public override void BackButtonPressed()
         {
             base.BackButtonPressed();
-            LocationSensor.Instance.Stop(LocationSensors.GPS);
+            Syderis.CellSDK.IO.LocationSystem.LocationSensor.Instance.Stop(Syderis.CellSDK.Common.LocationSensor.GPS);
             Program.Instance.Exit();
         }
     }
