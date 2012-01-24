7d1e84482f272ee970f4f71270f9e86e20768818
jfernandez <jfernandez@syderis.com>
2012-01-24 15:50:53 +0100

    Fixed Android Progress Bar Tutorial

--- ProgressBar/Sources/Application.cs
+++ ProgressBar/Sources/Application.cs
@@ -1,77 +1,50 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+#region Using Statements
+using Microsoft.Xna.Framework;
 using Syderis.CellSDK.Core;
 using Syderis.CellSDK.Core.Controls;
-using Microsoft.Xna.Framework;
+#endregion
 
 namespace ProgressBarSample
 {
     public class Application : MobileApplication
     {
-
         private ProgressBar charger;
-
         private bool charged;
 
         public override void Initialize()
         {
-
             base.Initialize();
 
             Label message = new Label("Charging...");
-
             AddComponent(message, 0, 100);
 
             Button shot = new Button("Shot!");
-
             shot.Pressed += delegate
             {
-
                 if (charged)
                 {
-
                     charger.Value = 0;
-
                     charged = false;
-
                     message.Text = "Charging...";
-
                 }
-
             };
-
             AddComponent(shot, 150, 100);
 
             charger = new ProgressBar();
-
             charger.Value = 0;
-
             charger.EndEvent += delegate
             {
-
                 charged = true;
-
                 message.Text = "Charged!";
-
             };
-
             AddComponent(charger, 0, 0);
-
         }
 
         public override void Update(GameTime gameTime)
         {
-
             base.Update(gameTime);
-
             if (!charged)
-
                 charger.Value++;
-
         }
-
     }
 }
