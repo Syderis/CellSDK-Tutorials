e4c99378b53ddba2268cc044124c6af793aac65e
jfernandez <jfernandez@syderis.com>
2012-01-24 10:40:23 +0100

    Fixed Program.cs, Removed unused References

--- Animations/AndroidAnimations/AndroidAnimations/Program.cs
+++ Animations/AndroidAnimations/AndroidAnimations/Program.cs
@@ -1,21 +1,16 @@
-using System;
-
+#region Using Statements
 using Android.App;
-using Android.Content;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 
 using Syderis.CellSDK.Android.Launcher;
-using Syderis.CellSDK.Core;
 using Syderis.CellSDK.Common;
-
+using Microsoft.Xna.Framework;
+#endregion
 
 namespace Animations
 {
     [Activity(Label = "AndroidAnimations", MainLauncher = true, Icon = "@drawable/icon")]
-    public class Program : Activity
+    public class Program : AndroidGameActivity
     {
 
         public static Program Instance;
@@ -27,6 +22,7 @@ namespace Animations
         {
             base.OnCreate(savedInstanceState);
 
+            Kernel.Activity = this;
             Kernel view = new Kernel(this);
             SetContentView(view.Window);
 
