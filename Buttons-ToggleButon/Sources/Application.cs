82482d50cdc3e6a9b25caf29315efe9ac1f33cd4
jfernandez <jfernandez@syderis.com>
2012-01-24 11:53:05 +0100

    Updated Application.cs

--- Buttons-ToggleButon/Sources/Application.cs
+++ Buttons-ToggleButon/Sources/Application.cs
@@ -2,7 +2,8 @@
 using Microsoft.Xna.Framework;
 using Syderis.CellSDK.Core;
 using Syderis.CellSDK.Core.Controls;
-using Syderis.CellSDK.Core.Graphics; 
+using Syderis.CellSDK.Core.Graphics;
+using Syderis.CellSDK.Core.Physics;
 #endregion
 
 namespace Buttons
@@ -23,11 +24,11 @@ namespace Buttons
             //Add your code here
 
             CreatePhysicWorld();
+
             Button btn = new Button("Push me");
-            AddComponent(btn, Width / 2, Height / 2);
+            AddComponent(btn, Width / 2, Height / 2, BodyShape.SQUARE, BodyType.DYNAMIC, Category.Cat1);
             btn.Pivot = Vector2.One / 2;
             btn.Pressed += new Component.ComponentEventHandler(btn_Pressed);
-
         }
 
         void btn_Pressed(Component source)
@@ -36,26 +37,26 @@ namespace Buttons
             {
                 ball = new Button(Image.CreateImage("ball"), Image.CreateImage("ballpressed"));
                 ball.Pivot = Vector2.One / 2;
-                AddComponent(ball, Width/2, Height /5);
+                AddComponent(ball, Width / 2, Height / 5, BodyShape.SQUARE, BodyType.DYNAMIC, Category.Cat1);
                 ball.Draggable = true;
                 ball.Released += new Component.ComponentEventHandler(ball_Released);
             }
-
         }
+
         void ball_Released(Component source)
         {
             if (secondball == null)
             {
                 secondball = new Button(Image.CreateImage("ball"), Image.CreateImage("ballpressed"));
                 secondball.Pivot = Vector2.One / 2;
-                AddComponent(secondball, Width/2, 4 * Height / 5);
+                AddComponent(secondball, Width / 2, 4 * Height / 5, BodyShape.SQUARE, BodyType.DYNAMIC, Category.Cat1);
                 secondball.Draggable = true;
             }
         }
-         public override void BackButtonPressed()
+        
+        public override void BackButtonPressed()
         {
             Program.Instance.Exit();
         }
-
     }
 }
