318f947d03c1b1fe3b26a70f6a458fc32773fb31
jfernandez <jfernandez@syderis.com>
2012-01-24 10:46:22 +0100

    Fixed Program.cs, Fixed Android Project Preferences, Removed unused References

--- Basics/Sources/Basics/Application.cs
+++ Basics/Sources/Basics/Application.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
 using Syderis.CellSDK.Core;
-using Syderis.CellSDK.Core.Controls;
 using Syderis.CellSDK.Core.Graphics;
 
 namespace Basics
