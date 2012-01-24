e6312b7be1b4592a9ae18244df50125a6fbd4908
jfernandez <jfernandez@syderis.com>
2012-01-24 16:13:33 +0100

    Fixed Android Slider Tutorial

--- Slider/Sources/Application.cs
+++ Slider/Sources/Application.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+#region Using Statements
+using Microsoft.Xna.Framework;
 using Syderis.CellSDK.Core;
 using Syderis.CellSDK.Core.Controls;
-using Microsoft.Xna.Framework;
+#endregion
 
 namespace Slider_Sample
 {
@@ -23,21 +20,16 @@ namespace Slider_Sample
 
             // TODO: Replace these comments with your own poetry, and enjoy!
             slider = new Slider();
-
             value = new Label("");
 
             AddComponent(value, 75f, 400f);
-
             AddComponent(slider, 100f, 200f);
         }
 
         public override void Update(GameTime gameTime)
         {
-
             base.Update(gameTime);
-
             value.Text = slider.Value.ToString();
-
         }
 
         public override void BackButtonPressed()
