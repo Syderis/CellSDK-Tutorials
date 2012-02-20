563c736c5a164c539723b0ece1681aa41ea410df
Jorgemagic <jcanton@syderis.com>
2012-02-20 14:33:49 +0100

    - Update to Version 1.1 WP7

--- UIConcepts/Canvas/Sources/Application.cs
+++ UIConcepts/Canvas/Sources/Application.cs
@@ -1,57 +1,35 @@
+/*
+ * Copyright 2012 Syderis Technologies S.L. All rights reserved.
+ * Use is subject to license terms.
+ */
+
+#region Using Statements
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Syderis.CellSDK.Core;
 using Syderis.CellSDK.Core.Controls;
 using Syderis.CellSDK.Core.Graphics;
-using Syderis.CellSDK.Common;
+using Syderis.CellSDK.Common; 
+#endregion
 
 namespace CanvasSample
 {
     class Application : MobileApplication
-    {
-        List<Label> colors;
-        MyCanvas canv;
+    {    
         public override void Initialize()
         {
 
-            SetBackground(Image.CreateImage("Background"), Adjustment.NONE);
+            StaticContent.Graphics.IsFullScreen = true;
+            StaticContent.Graphics.ApplyChanges();
 
-            canv = new MyCanvas(Preferences.Width, Preferences.Height);
-            canv.BringToFront = false;
-            AddComponent(canv, 0, 0);
+            StaticContent.ScreenManager.GoToScreen(new MainScreen());
 
-            colors = new List<Label>();
-            Image img = Image.CreateImage("color");
-            Image aux = img.SubImage(0, 0, img.Width, img.Height);
-            aux.Color = Color.Red;
-            colors.Add(new Label(aux));
-            aux = img.SubImage(0, 0, img.Width, img.Height);
-            aux.Color = Color.Gray;
-            colors.Add(new Label(aux));
-            aux = img.SubImage(0, 0, img.Width, img.Height);
-            aux.Color = Color.Yellow;
-            colors.Add(new Label(aux));
-            aux = img.SubImage(0, 0, img.Width, img.Height);
-            aux.Color = Color.Blue;
-            colors.Add(new Label(aux));
+        }       
 
-            for (int i = 0; i < colors.Count; i++)
-            {
-                AddComponent(colors[i], i * colors[i].Size.X, Preferences.Height - colors[i].Size.Y);
-                colors[i].Released -= Application_Released;
-                colors[i].Released += new Component.ComponentEventHandler(Application_Released);
-
-            }
-
-        }
-
-        void Application_Released(Component source)
+        public override void Exit()
         {
-            canv.PaintColor = ((Label)source).Image.Color;
-        }
+            base.Exit();
 
-         public override void BackButtonPressed()
-        {
             Program.Instance.Exit();
         }
     }
