438f31d2d3ea257b0cd9014915fb02d38641956a
davilovkick <davila@syderis.com>
2012-02-21 12:34:12 +0100

    - Update Animation projects for WP7 and Android to 1.1

--- Animation System/Animations/WP7Animations/WP7Animations/Program.cs
+++ Animation System/Animations/WP7Animations/WP7Animations/Program.cs
@@ -1,7 +1,13 @@
+/*
+ * Copyright 2012 Syderis Technologies S.L. All rights reserved.
+ * Use is subject to license terms.
+ */
+
+#region Using Statements
 using System;
 
 using Syderis.CellSDK.WindowsPhone.Launcher;
-
+#endregion
 
 namespace Animations
 {
@@ -17,8 +23,6 @@ namespace Animations
             base.Application = application;
             base.Initialize();
         }
-
-        
     }
 }
 
