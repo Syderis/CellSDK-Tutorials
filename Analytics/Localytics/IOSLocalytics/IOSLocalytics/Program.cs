f43add866150bf2b1d33795933221ee7b92341c4
David Ávila Membrives <davila@syderis.com>
2012-02-29 15:12:44 +0100

    - Uploaded the Localytics tutorial for IOS

---
+++ Analytics/Localytics/IOSLocalytics/IOSLocalytics/Program.cs
@@ -0,0 +1,47 @@
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+using Syderis.CellSDK.IOS.Launcher;
+
+namespace Localytics
+{
+	[Register("AppDelegate")]
+	class Program : UIApplicationDelegate
+	{
+		public static Program Instance;
+		private Kernel kernel;
+		
+		static void Main (string[] args)
+		{
+			UIApplication.Main (args, null, "AppDelegate");
+		}
+
+		public override void FinishedLaunching (UIApplication app)
+		{
+			Instance = this;
+			Application application = new Application ();
+			kernel = new Kernel (application);
+			kernel.Run ();
+		}
+
+		public void Exit ()
+		{
+			kernel.Exit ();
+		}
+		
+		public override void DidEnterBackground (UIApplication application)
+		{
+			kernel.OnDeactivated ();
+		}
+		
+		public override void WillEnterForeground (UIApplication application)
+		{
+			kernel.OnActivated ();
+		}
+		
+		public override void WillTerminate (UIApplication application)
+		{
+			kernel.OnExiting ();
+		}
+	}
+}
