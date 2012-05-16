dd1cecb3e84fbe1ddf08141c5d480c96a42b100e
Juan Maria Lao <juanlao@gmail.com>
2012-05-16 16:33:44 +0200

    I18N tutorial

---
+++ Basic Concepts/Localization/WP7LocalizedStrings/WP7LocalizedStrings/Program.cs
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2011 Syderis Technologies S.L. All rights reserved.
+ * Use is subject to license terms.
+ */
+
+#region Using statements
+using System;
+
+using Syderis.CellSDK.WindowsPhone.Launcher;
+using System.Threading;
+#endregion
+
+namespace LocalizedStrings
+{
+    public class Program : Kernel
+    {
+
+        #region Variables
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Public methods
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        protected override void Initialize()
+        {
+         
+            Application application = new Application();
+            FramesPerSecond = 50;
+            base.Application = application;
+            base.Initialize();
+        }
+        #endregion
+
+        #region Private methods
+        #endregion
+    }
+}
+
