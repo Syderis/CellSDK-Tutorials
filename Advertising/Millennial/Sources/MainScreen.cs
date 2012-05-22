697c714b2e8d8be666b49db78491a6f91c1639f5
Jorgemagic <jcanton@syderis.com>
2012-05-22 15:07:41 +0200

    Update to 1.3

--- Advertising/Millennial/Sources/MainScreen.cs
+++ Advertising/Millennial/Sources/MainScreen.cs
@@ -32,16 +32,27 @@ namespace Millenial
         public override void Initialize()
         {
             base.Initialize();
+            
             SetBackground(Color.Blue);
-            // TODO: Replace these comments with your own poetry, and enjoy!
+            
             AdsConfig cfg= new AdsConfig("push your code",1);
+
             IAdvertising ads = AdvertisingFactory.CreateAds(AdvertisingType.MILLENNIAL,cfg);
             ads.Test = true;
-            Banner milleniall = new Banner(ads, 300, 100,"127.0.0.1");
-            AddComponent(milleniall,
-                Preferences.Width / 2 - milleniall.Width / 2,
-                Preferences.Height / 2 - milleniall.Height / 2);
 
+            Banner millennial = new Banner(ads, 300, 50,"127.0.0.1");
+            millennial.DefaultImage = ResourceManager.CreateImage("banner_zombeee");
+            millennial.EventClick += new Banner.DefaultClick(millennial_EventClick);
+
+            AddComponent(millennial,
+                Preferences.Width / 2 - millennial.Width / 2,
+                Preferences.Height / 2 - millennial.Height / 2);
+
+        }
+
+        void millennial_EventClick(object sender)
+        {
+            Preferences.App.Alert("Click on default banner", "Custom action");
         }
 
         public override void BackButtonPressed()
